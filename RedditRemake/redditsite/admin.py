32b936483eba2fc5a0b9113c1e0cb936435b8b81
Aaron Scruggs <aarondscruggs@gmail.com>
2016-03-24 17:37:16 -0700

    Working well, normal mode almost done

--- RedditRemake/redditsite/admin.py
+++ RedditRemake/redditsite/admin.py
@@ -3,11 +3,12 @@ from redditsite.models import Subreddit, Post, Comment
 
 @admin.register(Subreddit)
 class SubredditAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "description", "created_at")
+    list_display = ("id", "title", "description", "created_at", "post_count",
+                    "today_count", "daily_average")
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "description", "url", "slug")
+    list_display = ("id", "title", "description", "url", "slug", "is_hot")
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
--- RedditRemake/redditsite/models.py
+++ RedditRemake/redditsite/models.py
@@ -1,7 +1,10 @@
+import datetime
+
 from django.db import models
 from django.core.validators import MinLengthValidator
 from django import forms
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 class Subreddit(models.Model):
@@ -10,59 +13,67 @@ class Subreddit(models.Model):
     description = models.CharField(max_length=250)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def current_count(self):
+    @property
+    def post_count(self):
         # returns count of total posts
-        pass
+        return self.post_set.count()
 
+    @property
     def today_count(self):
-        # returns posts within last 24 hours
-        pass
+        # returns post count within last 24 hours
+        one_day_ago = timezone.now() - datetime.timedelta(days=1)
+        return self.post_set.filter(created_at__gte=one_day_ago).count()
 
+    @property
     def daily_average(self):
         # average count of posts over the last 7 days
-        pass
+        one_week_ago = timezone.now() - datetime.timedelta(days=7)
+        return self.post_set.filter(created_at__gte=one_week_ago).count() / 7
 
     def __str__(self):
-        # name
-        pass
+        return self.title
 
 
 class Post(models.Model):
 
     title = models.CharField(max_length=100)
-    description = models.CharField(
-        max_length=1000,
-        validators=[MinLengthValidator(256)]
-    )
-    url = forms.URLField(required=False)
-    slug = models.SlugField()  # default of 50 is fine.
+    description = models.CharField(max_length=1000)
+        #help_text="Must be over 255 characters") #,validators=[MinLengthValidator(256)]
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    url = forms.URLField(required=False) # add print
+    slug = models.SlugField()  # default of 50 is fine. Required?
+
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
     modified_at = models.DateTimeField(auto_now=True)
 
     user = models.ForeignKey(User)
     subreddit = models.ForeignKey(Subreddit)
 
-    def is_recent(self):
-        # True if created in the last 24 hours, else False
-        pass
-
+    @property
     def is_hot(self):
         # True if post has gotten more than 3 comments in the past 3 hours.
-        pass
+        three_hours_ago = timezone.now() - datetime.timedelta(hours=3)
+        recent = self.comment_set.filter(created_at__gte=three_hours_ago).count()
+        return recent >= 3
+
+    def __str__(self):
+        return "title: {}, created: {}".format(self.title, self.created_at)
 
 
 class Comment(models.Model):
 
     comment_text = models.CharField(
-        max_length=1000,
-        validators=[MinLengthValidator(256)]
-    )
+        max_length=1000)
+    #     help_text="Must be over 255 characters",
+    #     validators=[MinLengthValidator(256)]
+    # )
 
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
     modified_at = models.DateTimeField(auto_now=True)
 
     user = models.ForeignKey(User)
     post = models.ForeignKey(Post)
+
 
 
